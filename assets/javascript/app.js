8a6f27bdcd2a2470c24b8c2cc433f821cbbd82f0
Brian Burnside <brianburnside@Brians-MacBook-Air.local>
2016-05-03 20:30:56 -0400

    added some js

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	var game = 	{
+
+		questions:
+				[
+				{
+				question: "What motocross rider is featured on the 2016 Wheaties cereal box?",
+				answer0: "James Stewart",
+				answer1: "Ricky Carmichael",
+				answer2: "Travis Pastrana",
+				answer3: "Ryan Dungey",
+				answer4: "Yo greedy, greedy grandpappy",
+				correctans: "Ryan Dungey",
+				}
+				]
+
+				}
+
+				correctans: 0,
+				wrongans: 0,
+				unans: 0, 
+
+
+	// start: function(){
+	// 	alert:"you started"
+	// }
+
+    $("#start").on("click", function(){
+        console.log("you started");
+    })
+})
